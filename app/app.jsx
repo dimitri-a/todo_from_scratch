dadecea057a5a92bc45ba3e6680993884b5fbf72
dimitric <couwenberg@gmail.com>
2017-09-19 13:32:43 +1000

    first

--- app/app.jsx
+++ app/app.jsx
@@ -17,7 +17,7 @@ const store = createStore(reducer)
 
 const App = () => (
     <Provider store={store}>
-        <div>
+        <div>kfjsdklfjslkdj
            <Nothing></Nothing>
         </div>
     </Provider>
