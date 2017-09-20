59c942adfb8644f82a5ea72d7f6b8dca0a7fe3ae
dimitric <couwenberg@gmail.com>
2017-09-20 17:00:57 +1000

    unit test failing

--- app/components/Invoer.test.js
+++ app/components/Invoer.test.js
@@ -12,11 +12,8 @@ it('does it render and trigger saveTodo event when clicking on button', () => {
     const output = renderer.getRenderOutput()
 
     //set text value
-   // output.props.children[0].value ='hoera';
+    //output.props.children[0].value = 'hoera';
 
-    console.log(output.props);
-
-    //todo find element that contains the event connected to saveTodo in this case onClick on button
     output.props.children[1].props.onClick();
 
     expect(props.saveTodo).toBeCalled()
